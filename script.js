defce93725bb4fad7b03f3edfdc2d874dce65419
Shubhamparekh04 <shubhamparekh04@gmail.com>
2024-06-19 23:36:31 +0530

    Password reset validation

--- script.js
+++ script.js
@@ -1,37 +1,20 @@
 $(document).ready(function () {
-    // $("#resetPwdBtn").on("click", function () {
-    //     if ($("#floatingPassword").val() == $("#floatingCnfPassword").val()) {
-
-    //         if ($("#floatingPassword").val() != "" || $("#floatingCnfPassword").val() != "") {
-    //             alert(`Kindly fill the data`);
-    //             location.reload();
-    //         } else {
-    //             alert(`Password reset success fully`);
-    //             location.reload();
-    //         }
-    //     } else {
-    //         alert(` Both password should match`);
-    //         location.reload();
-    //     }
-    // });
-});
-
-
-
-$("#resetPwdBtn").on("click", function () {
-    if ($("#floatingPassword").val() == "" && $("#floatingCnfPassword").val() == "") {
-        alert(`Kindly fill the data`);
-        location.reload();
-    } else {
-        if ($("#floatingPassword").val() == $("#floatingCnfPassword").val()) {
-            alert(`Password reset success fully`);
+    $("#resetPwdBtn").on("click", function () {
+        if ($("#floatingInput").val() == "" || $("#floatingPassword").val() == "" || $("#floatingCnfPassword").val() == "") {
+            alert(`Kindly fill the data`);
             location.reload();
         } else {
-            alert(` Both password should match`);
-            location.reload();
+            if ($("#floatingPassword").val() == $("#floatingCnfPassword").val()) {
+                alert(`Password reset successfully`);
+                location.reload();
+            } else {
+                alert(` Both password should match`);
+                location.reload();
+            }
         }
-    }
+    });
 });
+
 
 
 
